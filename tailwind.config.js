af11b3bb3f7ffddfda3fc41c6d0c331773cdcce9
Prateek Jain <77361820+letscodeprateek@users.noreply.github.com>
2021-02-07 15:27:09 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily:{
+      gotham:['Gotham Pro','sans-serif']
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+      xxl: '1590px',
+    },
+  colors:{
+    black:'#000000',
+
+     Yellow: '#FFF512',
+     Gargoyle: '#FFDE40',
+     yellowa: '#EBB014',
+     gold: '#DE8D00',
+     brown:'#9C4100',
+     blacka:'#090907'
+
+
+  },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
